3b3739d6e5f0cd09f68590194eff02153169c379
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-03-05 18:18:05 +0100

    strax final touch

--- src/views/AlbumView.vue
+++ src/views/AlbumView.vue
@@ -1,26 +1,38 @@
 <template>
   <header>
     <!-- $route.params här: { "userId": "1", "albumId": "1" } -->
-    <h2>{{ albums[$route.params.albumId - 1].title || albums[lastLetter - 1].title }}</h2>
+    <h2>{{ albums[$route.params.albumId - 1].title }}</h2>
     <p>{{ albums.length }} photos</p>
   </header>
   <div class="photos">
-    <div class="photo" v-for="album in albums">
-      <img src="" :alt="album.title">
-      <p>600 x 600</p>
+    <div class="photo" v-for="(album, index) in albums" @click="albumId(index)">
+      <img src="../assets/helloWorld.jpg" :alt="album.title">
+      <!-- <p>600 x 600</p> -->
     </div>
+  </div>
+  <div class="popup">
+    <span>&times;</span>
+    <img src="../assets/helloWorld.jpg" :alt=altPlaceholder.value>
   </div>
 </template>
 
 <script>
 import { storeToRefs } from 'pinia'
+import { ref } from 'vue'
 import { useEmployeeStore } from '../stores/EmpStore'
 export default {
   setup() {
     const EmpStore = useEmployeeStore()
     const { employees, albums } = storeToRefs(EmpStore)
 
-    return { EmpStore, employees, albums }
+    let altPlaceholder = ref("")
+    function albumId(indexet) {
+      altPlaceholder.value = this.albums[indexet].title;
+      console.log(this.albums[indexet].title)
+      console.log(altPlaceholder.value)
+    }
+
+    return { EmpStore, employees, albums, altPlaceholder, albumId }
   }
 }
 </script>
